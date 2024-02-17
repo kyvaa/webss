dbc2fbcbc4ace24ce95aeeae5152020377ced7ae
kyvaa <145992972+kyvaa@users.noreply.github.com>
2024-02-17 10:37:12 +0700

    Add files via upload

---
+++ akutansi.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+<title>SMK WIRASABA</title>
+<!-- Meta tag Keywords -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Scholarly web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
+function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!--// Meta tag Keywords -->
+<!-- css files -->
+<link rel="stylesheet" href="css/bootstrap.css"> <!-- Bootstrap-Core-CSS -->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" /> <!-- Style-CSS --> 
+<link rel="stylesheet" href="css/font-awesome.css"> <!-- Font-Awesome-Icons-CSS -->
+<link rel="stylesheet" href="css/swipebox.css">
+<link rel="stylesheet" href="css/roma.css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet">
+<link rel="stylesheet" href="css/jquery-ui.css" />
+<!-- //css files -->
+<!-- online-fonts -->
+<link href="//fonts.googleapis.com/css?family=Exo+2:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&amp;subset=cyrillic,latin-ext" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+<!-- //online-fonts -->
+</head>
+<body>
+
+<?php include ('napigasi.php'); ?>
+
+<div class="clearfix"> </div> 
+
+<!-- Slideshoww -->	
+<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img width="1500" height="500" src="images/akun/ap11.jpg"alt="...">
+      <div class="carousel-caption">
+	  <h1><b>Asper</b> </h1>
+              <p>"Praktiknya Asisten Perawat adalah untuk membantu seorang perawat yang telah mempunyai lisensi (STR) dalam bertugas seperti membantu informasi pengkajian pasien, perencanaan asuhan, melakukan evaluasi keadaan dan kebutuhan pasien.".</p>
+      </div>
+    </div>
+    <div class="item">
+      <img width="1500" height="500" src="images/akun/ap22.jpg" alt="...">
+      <div class="carousel-caption">
+	  <h1><b>Asper</b> </h1>
+              <p>Asisten perawat adalah orang yang bertugas membantu pekerjaan keperawatan orang yang sakit. Disebut asisten, karena, orang yang bertugas utama merawat, sudah ada. Asisten ini hanya membantu tugas perawat pada saat-saat tertentu saja..</p>
+      </div>
+    </div>
+	<div class="item">
+      <img  width="1500" height="500" src="images/akun/ap33.png" alt="...">
+      <div class="carousel-caption">
+	  <h1><b>Asper</b> </h1>
+              <p>Jurusan asisten keperawatan merupakan program yang bertujuan untuk menghasilkan tenaga ahli dalam asistensi keperawatan yang terampil dan kompeten di bidang pelayanan dasar keperawatan khususnya pemenuhan kebutuhan dasar manusia.</p>
+      </div>
+    </div>
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!-- //Slideshoww -->
+  
+<!-- footer -->
+<div class="w3layouts_copy_right">
+	<div class="container">
+		<p>© 2018 SMK WIRASABA | Design by <a href="">kemzy.</a></p>
+	</div>
+</div>
+<!-- //footer -->
+
+<!-- js-scripts -->			
+<!-- js-files -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script> <!-- Necessary-JavaScript-File-For-Bootstrap --> 
+<!-- //js-files -->
+<!-- Baneer-js -->
+
+
+<!-- smooth scrolling -->
+<script src="js/SmoothScroll.min.js"></script>
+<!-- //smooth scrolling -->
+<!-- stats -->
+<script type="text/javascript" src="js/numscroller-1.0.js"></script>
+<!-- //stats -->
+<!-- moving-top scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+	<script type="text/javascript">
+		$(document).ready(function() {
+		/*
+			var defaults = {
+			containerID: 'toTop', // fading element id
+			containerHoverID: 'toTopHover', // fading element hover id
+			scrollSpeed: 1200,
+			easingType: 'linear' 
+			};
+		*/								
+		$().UItoTop({ easingType: 'easeOutQuart' });
+		});
+	</script>
+	<a href="#home" class="scroll" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!-- //moving-top scrolling -->
+<!-- gallery popup -->
+<script src="js/jquery.swipebox.min.js"></script> 
+<script type="text/javascript">
+jQuery(function($) {
+	$(".swipebox").swipebox();
+});
+</script>
+<!-- //gallery popup -->
+<!--/script-->
+	<script src="js/simplePlayer.js"></script>
+			<script>
+				$("document").ready(function() {
+					$("#video").simplePlayer();
+				});
+			</script>
+<!-- //Baneer-js -->
+<!-- Calendar -->
+<script src="js/jquery-ui.js"></script>
+	<script>
+	  $(function() {
+		$( "#datepicker" ).datepicker();
+	 });
+	</script>
+<!-- //Calendar -->	
+
+<!-- //js-scripts -->
+</body>
+</html>
